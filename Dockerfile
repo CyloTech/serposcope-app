7757f717256d3c671044fd20d8c5f18e9f27f376
Glow <gavin@cylo.io>
2018-08-21 12:44:47 +0100

    Initial Commit.

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,25 @@
-################################################################################
-# Base image
-################################################################################
-
 FROM java:8-jre
 
-################################################################################
-# Build instructions
-################################################################################
-
-# Envronment variables
-ENV SERPOSCOPE_VERSION=2.7.1
+ENV SERPOSCOPE_VERSION=2.9.0
+ENV SERPOSCOPE_PORT=80
+ENV ADMIN_EMAIL=test@test.com
+ENV ADMIN_PASS=test123
 
 # Install packages
 RUN apt-get update && apt-get install -my \
     supervisor \
+    net-tools \
     \
     && rm /etc/timezone \
-    && echo "Europe/Paris" > /etc/timezone \
+    && echo "Europe/London" > /etc/timezone \
     && chmod 644 /etc/timezone \
     && dpkg-reconfigure -f noninteractive tzdata \
     \
 	&& apt-get purge -y --auto-remove \
 	&& apt-get clean \
 	&& rm -rf /var/lib/apt/lists/*
+
+RUN setcap cap_net_bind_service=+ep /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
 RUN wget https://serposcope.serphacker.com/download/${SERPOSCOPE_VERSION}/serposcope_${SERPOSCOPE_VERSION}_all.deb -O /tmp/serposcope.deb \
     && dpkg --force-confold -i /tmp/serposcope.deb \
@@ -35,20 +32,8 @@ COPY serposcope /etc/default/serposcope
 
 RUN chmod 755 /docker-entrypoint.sh
 
-################################################################################
-# Volumes
-################################################################################
-
 VOLUME /var/lib/serposcope/
 
-################################################################################
-# Ports
-################################################################################
-
-EXPOSE 7134
-
-################################################################################
-# Entrypoint
-################################################################################
+EXPOSE 80
 
 ENTRYPOINT ["/usr/bin/supervisord", "-c", "/etc/supervisor/supervisord.conf"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,35 +1,60 @@
-#! /bin/bash
+#!/bin/bash
+set -x
 
-conf_file="/etc/serposcope.conf"
+if [ ! -f /etc/app_configured ]; then
 
-usermod -u 1000 serposcope
-groupmod -g 50 staff
-chown -R serposcope:staff /var/lib/serposcope
-chown -R serposcope:staff /var/log/serposcope
+    EMAIL_ENC=`sed "s/@/%40/g" <<<"$ADMIN_EMAIL"`
 
-function replace_param {
-  sed -i -r -e "/^# *${1}=/ {s|^# *||;s|=.*$|=|;s|$|$(eval echo \$$2)|}" $conf_file
-}
+    conf_file="/etc/serposcope.conf"
 
-if [ -n "$SERPOSCOPE_DB_URL" ]
-then
-  replace_param "serposcope.db.url" "SERPOSCOPE_DB_URL"
+    usermod -u 1000 serposcope
+    groupmod -g 50 staff
+    chown -R serposcope:staff /var/lib/serposcope
+    chown -R serposcope:staff /var/log/serposcope
+
+    function replace_param {
+      sed -i -r -e "/^# *${1}=/ {s|^# *||;s|=.*$|=|;s|$|$(eval echo \$$2)|}" $conf_file
+    }
+
+    if [ -n "$SERPOSCOPE_DB_URL" ]
+    then
+      replace_param "serposcope.db.url" "SERPOSCOPE_DB_URL"
+    else
+      echo "SERPOSCOPE_DB_URL is not set, keeping the default value"
+    fi
+
+    if [ -n "$SERPOSCOPE_DB_OPTIONS" ]
+    then
+      replace_param  "serposcope.db.options" "SERPOSCOPE_DB_OPTIONS"
+    else
+      echo "SERPOSCOPE_DB_OPTIONS not set, keeping the default value"
+    fi
+
+    if [ -n "$SERPOSCOPE_DB_DEBUG" ]
+    then
+    replace_param  "serposcope.db.debug" "SERPOSCOPE_DB_DEBUG"
+    else
+      echo "SERPOSCOPE_DB_DEBUG not set, keeping the default value"
+    fi
+
+    if [ -n "$SERPOSCOPE_PORT" ]
+    then
+      replace_param "serposcope.listenPort" "SERPOSCOPE_PORT"
+    else
+      echo "SERPOSCOPE_PORT is not set, keeping the default value"
+    fi
+
+    service serposcope start
+
+    sleep 5
+    DATA_STR="email=${EMAIL_ENC}&email-confirm=${EMAIL_ENC}&password=${ADMIN_PASS}&password-confirm=${ADMIN_PASS}"
+    curl -v 'http://127.0.0.1/create-admin' --data ${DATA_STR} --compressed &> /var/log/serpuser.log
+
+    # Finish App install.
+    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
+    touch /etc/app_configured
+
 else
-  echo "SERPOSCOPE_DB_URL is not set, keeping the default value"
+    service serposcope start
 fi
-
-if [ -n "$SERPOSCOPE_DB_OPTIONS" ]
-then
-  replace_param  "serposcope.db.options" "SERPOSCOPE_DB_OPTIONS"
-else
-  echo "SERPOSCOPE_DB_OPTIONS not set, keeping the default value"
-fi
-
-if [ -n "$SERPOSCOPE_DB_DEBUG" ]
-then
-replace_param  "serposcope.db.debug" "SERPOSCOPE_DB_DEBUG"
-else
-  echo "SERPOSCOPE_DB_DEBUG not set, keeping the default value"
-fi
-
-service serposcope start && tail -F /var/log/serposcope/startup.log
+tail -F /var/log/serposcope/startup.log
